b621a2f04b77c3e2514a3e2e1e60515c7ba3358e
2415035 <i2415035@continental.edu.pe>
2024-11-27 02:50:29 +0000

    prueba6

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from supabase_config import supabase_client
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
 import streamlit as st
-import numpy as np
 
 # Cargar datos de Supabase
 response = supabase_client.table('envios').select('*').execute()
@@ -17,6 +16,9 @@ regiones = pd.DataFrame(response_regiones.data)
 envios['fecha_envio'] = pd.to_datetime(envios['fecha_envio'])
 envios['mes'] = envios['fecha_envio'].dt.month
 
+# Verificar las columnas iniciales
+st.write("Columnas en el DataFrame de envios:", envios.columns)
+
 # Crear un diccionario que mapea el id_region a nombre de la región
 id_region_to_nombre = dict(zip(regiones['id_region'], regiones['nombre_region']))
 
@@ -27,8 +29,22 @@ region = st.selectbox('Región', region_names)
 # Convertir el nombre de la región seleccionado de vuelta al id_region
 region_id = [key for key, value in id_region_to_nombre.items() if value == region][0]
 
-# Codificar las columnas categóricas si es necesario
-envios['id_region_encoded'] = envios['id_region'].map(id_region_to_nombre)
+# Verificar si las columnas de codificación existen
+if 'id_evento' in envios.columns and 'id_tipo_servicio' in envios.columns and 'id_ruta' in envios.columns:
+    # Codificar las columnas categóricas
+    label_encoder_evento = LabelEncoder()
+    envios['id_evento_encoded'] = label_encoder_evento.fit_transform(envios['id_evento'])
+
+    label_encoder_servicio = LabelEncoder()
+    envios['id_tipo_servicio_encoded'] = label_encoder_servicio.fit_transform(envios['id_tipo_servicio'])
+
+    label_encoder_ruta = LabelEncoder()
+    envios['id_ruta_encoded'] = label_encoder_ruta.fit_transform(envios['id_ruta'])
+else:
+    st.write("Faltan columnas necesarias para la codificación.")
+
+# Verificar las columnas después de la codificación
+st.write("Columnas después de la codificación:", envios.columns)
 
 # Definir las características y el objetivo
 features = ['id_region_encoded', 'cantidad_envios', 'id_evento_encoded', 'id_tipo_servicio_encoded', 'id_ruta_encoded', 'mes']
