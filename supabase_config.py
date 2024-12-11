6ef6e787b9a29289d0d4253c82a04dc3b9685253
2415035 <i2415035@continental.edu.pe>
2024-12-11 01:15:22 +0000

    hola mundo

--- supabase_config.py
+++ supabase_config.py
@@ -1,7 +1,7 @@
 import supabase
 
 # Configurar la conexión a Supabase
-SUPABASE_URL = 'https://sjlphygbvmlrvcidfkoc.supabase.co'  # Cambia esto por tu URL de Supabase
+SUPABASE_URL = 'https://sjlphygbvmlrvcidfkoc.supabase.co' 
 SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqbHBoeWdidm1scnZjaWRma29jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIzMzI0MTAsImV4cCI6MjA0NzkwODQxMH0.GC6jZ8OcDlza6t2W9uHjpALJ9kMLjUNGfSaT97OUL84'  # Obtén tu clave API desde tu proyecto en Supabase
 
 # Crear cliente de conexión
